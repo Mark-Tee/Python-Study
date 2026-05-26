f93e6ad1157684549b2520a19c540b467eae1da1
Dommy_Hsu <1248849066@qq.com>
2026-05-26 17:09:00 +0800

    九九乘法表

--- demo.py
+++ demo.py
@@ -1,9 +1,9 @@
 i = 1
 j = 1
-while i<=3:
-    print(f"内循环{i}次")
-    i +=1
-    while j<=5: 
-        print(f"这是第{j}次内循环")
-        j +=1
+while i <= 9:
+    while j <= i:
+        print(f"{i}*{j}={i*j}\t", end="")
+        j += 1
+    print()  # 换行
+    i += 1
     j = 1  # 重置内循环计数器
