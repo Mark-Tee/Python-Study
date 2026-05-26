061cf2ad23b2b69720067c2d11d10189ea015b1d
Dommy_Hsu <1248849066@qq.com>
2026-05-26 13:49:16 +0800

    内循环和外循环

--- demo.py
+++ demo.py
@@ -1,7 +1,9 @@
-a = 1
-# print(a)
-for i in range(5):
-    print(i)
-print("Hello, World!")
-print("Hello, World!")
-print("xuduo")
+i = 1
+j = 1
+while i<=3:
+    print(f"内循环{i}次")
+    i +=1
+    while j<=5: 
+        print(f"这是第{j}次内循环")
+        j +=1
+    j = 1  # 重置内循环计数器
