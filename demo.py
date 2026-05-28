1974b6919d8c8ddfbd49fef22ae79f52bad89515
Dommy_Hsu <1248849066@qq.com>
2026-05-28 19:22:31 +0800

    编码与解码

--- demo.py
+++ demo.py
@@ -1,15 +1,9 @@
-i = 1
-while i <= 10:
-    print(f"小明吃了{i}个苹果")
-    if i == 5:
-        print("小明吃饱了，不吃了")
-        i = 1
-        break
-    i += 1
-while i <= 10:
-    print(f"小明吃了{i}个苹果")
-    if i == 5:
-        print(f"第{i}个苹果，小明吃到虫子了")
-        i += 1
-        continue
-    i += 1
+str = "许多"
+stl = str.encode('utf-8')
+print(stl)
+stl2 = stl.decode('utf-8')
+print(stl2)
+#encode是将字符串转换为字节串，decode是将字节串转换为字符串
+#在Python中，字符串是以Unicode编码的，而字节串是以特定编码（如UTF-8）表示的二进制数据。
+#使用encode方法可以将字符串转换为字节串，而使用decode方法可以将字节串转换回字符串。
+#在这个例子中，字符串"许多"被编码为UTF-8字节串，然后再解码回字符串。
