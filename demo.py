5cca2d57f3d27cd2ad5d64602afc25160fe8f0b5
Dommy_Hsu <1248849066@qq.com>
2026-05-28 18:44:28 +0800

    小明吃苹果

--- demo.py
+++ demo.py
@@ -1,9 +1,15 @@
 i = 1
-j = 1
-while i <= 9:
-    while j <= i:
-        print(f"{i}*{j}={i*j}\t", end="")
-        j += 1
-    print()  # 换行
+while i <= 10:
+    print(f"小明吃了{i}个苹果")
+    if i == 5:
+        print("小明吃饱了，不吃了")
+        i = 1
+        break
     i += 1
-    j = 1  # 重置内循环计数器
+while i <= 10:
+    print(f"小明吃了{i}个苹果")
+    if i == 5:
+        print(f"第{i}个苹果，小明吃到虫子了")
+        i += 1
+        continue
+    i += 1
