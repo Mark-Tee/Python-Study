6dcade5b56879d41988cd67b7285b3ff9b176074
Dommy_Hsu <1248849066@qq.com>
2026-06-04 08:34:33 +0800

    修改-练习

--- 练习.py
+++ 练习.py
@@ -102,31 +102,79 @@
 用 while 循环 让程序一直运行，直到选 4 退出
 用 if/elif 处理菜单选择
 用 列表操作 完成增、查、删'''
-stuLi = ['张三', '李四', '王五']
-while True :
-    sel = int(input("\n1、查看所有学生\t2、添加学生\n3、删除学生\t4、退出程序\n"))
-    if sel == 1:
-        for i in stuLi :
-            print(i)
-    elif sel == 2:
-        adname = input("请输入需要添加的同学\n")
-        stuLi.append(adname)
-        print("成功添加\n")
-        continue
-    elif sel == 3:
-        delname = input("请输入需要删除的同学姓名\n")
-        if delname in stuLi :
-            stuLi.remove(delname)
-            print("成功删除\n")
-            continue
-        else :
-            print("不存在该同学\n")
-            continue
-    elif sel == 4:
-        print("退出\n")
-        break
-    else :
-        print("请输入正确选项\n")
-        continue
-            
+# stuLi = ['张三', '李四', '王五']
+# while True :
+#     sel = int(input("\n1、查看所有学生\t2、添加学生\n3、删除学生\t4、退出程序\n"))
+#     if sel == 1:
+#         for i in stuLi :
+#             print(i)
+#     elif sel == 2:
+#         adname = input("请输入需要添加的同学\n")
+#         stuLi.append(adname)
+#         print("成功添加\n")
+#         continue
+#     elif sel == 3:
+#         delname = input("请输入需要删除的同学姓名\n")
+#         if delname in stuLi :
+#             stuLi.remove(delname)
+#             print("成功删除\n")
+#             continue
+#         else :
+#             print("不存在该同学\n")
+#             continue
+#     elif sel == 4:
+#         print("退出\n")
+#         break
+#     else :
+#         print("请输入正确选项\n")
+#         continue
+
+# NameLi = []
+# addSel = "增加姓名"
+# delSel = "删除姓名"
+# chaSel = "修改姓名"
+# serSel = "查找姓名"
+
+# def SqlNameEasy ():
+#     Sel = input(f"1、{addSel}\t2、{delSel}\n3、{chaSel}\t4、{serSel}\n5、退出\n请输入选项：")
+#     if Sel == "1":
+#         Name = input("请输入姓名：")
+#         NameLi.append(Name)
+#         print(f"已添加姓名：{Name}")
+#         return SqlNameEasy()
+#     elif Sel == "2":
+#         Name = input("请输入要删除的姓名：")
+#         if Name in NameLi:
+#             NameLi.remove(Name)
+#             print(f"已删除姓名：{Name}")
+#         else:
+#             print("姓名不存在！")
+#         return SqlNameEasy()
+#     elif Sel == "3":
+#         OldName = input("请输入要修改的姓名：")
+#         if OldName in NameLi:
+#             NewName = input("请输入新的姓名：")
+#             index = NameLi.index(OldName)
+#             #index = NameLi.index(OldName) 获取旧姓名的索引位置
+#             NameLi[index] = NewName
+#             print(f"已修改姓名：{OldName} -> {NewName}")
+#         else:
+#             print("姓名不存在！")
+#         return SqlNameEasy()
+#     elif Sel == "4":
+#         Name = input("请输入要查找的姓名：")
+#         if Name in NameLi:
+#             print(f"找到了姓名：{Name}")
+#         else:
+#             print("姓名不存在！")
+#         return SqlNameEasy()
+#     elif Sel == "5":
+#         print("退出程序")
+#         return
+#     else:
+#         print("无效选项！")
+#         return SqlNameEasy()
+# SqlNameEasy ()    
+
+
 
